59dad2f31de2e77c98da554cf444ff572a6951a0
tkdlek11112 <tkdlek11112@gmail.com>
2021-09-15 00:38:17 +0900

    model view template 완성

--- jinstagram/views.py
+++ jinstagram/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from content.models import Feed
 
 
 class Main(APIView):
     def get(self, request):
-        return render(request, 'jinstagram/main.html')
+        feed_list = Feed.objects.all()
+        return render(request, 'jinstagram/main.html', context=dict(feed_list=feed_list))
