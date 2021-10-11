e80c833c809493b28bef05e37f54da9c02d083b4
tkdlek11112 <tkdlek11112@gmail.com>
2021-10-12 01:38:30 +0900

    allow 호스트 추가

--- jinstagram/settings.py
+++ jinstagram/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(ddgwrknj$82@u324e@$mhqx6w&+!3f$o+18de%kulzyo@ij01
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
