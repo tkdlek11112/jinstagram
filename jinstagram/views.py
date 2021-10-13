766320e0867338f0557c6d8fc89c3d07b5298da4
moonpower <tkdlek11112@gmail.com>
2021-10-13 22:34:48 +0900

    프로필 업데이트

--- jinstagram/views.py
+++ jinstagram/views.py
@@ -40,5 +40,42 @@ class Main(APIView):
 
 class Profile(APIView):
     def get(self, request):
-        return render(request, 'jinstagram/profile.html')
+        email = request.session.get('email', None)
+        if email is None:
+            return render(request, 'user/login.html')
 
+        user = User.objects.filter(email=email).first()
+        if user is None:
+            return render(request, 'user/login.html')
+
+        feed_object_list = Feed.objects.filter(email=email).order_by('-id')
+        print(feed_object_list)
+        print(email)
+        feed_list = []
+        row_feed_list = []
+        for feed in feed_object_list:
+            like_count = FeedLike.objects.filter(feed_id=feed.id, is_like=True).count()
+            is_like = FeedLike.objects.filter(feed_id=feed.id, is_like=True, email=email).exists()
+            reply_list = Reply.objects.filter(feed_id=feed.id)
+            row_feed_list.append(dict(
+                id=feed.id,
+                profile_image=feed.profile_image,
+                user_id=feed.user_id,
+                image=feed.image,
+                content=feed.content,
+                like_count=like_count,
+                is_like=is_like,
+                reply_list=reply_list
+            ))
+
+            if len(row_feed_list) == 3:
+                feed_list.append(dict(row_feed_list=row_feed_list))
+                row_feed_list = []
+
+        if len(row_feed_list) > 0:
+            feed_list.append(dict(row_feed_list=row_feed_list))
+
+        return render(request,
+                      'jinstagram/profile.html',
+                      context=dict(feed_list=feed_list,
+                                   user=user))
